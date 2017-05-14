559d4a93d8b5579f394665987ec076f0ffd73703
Peter Keller <petekeller2@gmail.com>
2017-05-13 22:09:20 -0400

    group test

--- src/auth/groups.js
+++ src/auth/groups.js
@@ -1,27 +1,37 @@
 import fs from 'fs-extra';
 import epilogueAuth from './epilogueAuth';
+import utilities from '../utilities';
 
 export default {
   getJsonGroupPermissions(isTest, resourceName, groupResourceName, groupName, groupId) {
     let jsonPath = './groupPermissions.json';
     if (isTest) {
-      jsonPath = '../../test/testGroupPermissions.json';
+      jsonPath = 'test/testGroupPermissions.json';
     }
-    fs.readFile(jsonPath, 'utf8', (groupPermissionsErr, groupPermissionsData) => {
-      const groupPermissions = JSON.parse(groupPermissionsData);
-      const returnArray = [];
-      Object.entries(groupPermissions).forEach(([resource, resourceArray]) => {
-        if ((resource === resourceName) && (resourceArray[0] === groupResourceName)) {
-          if ((typeof resourceArray[2] === 'string') && (resourceArray[2].length > 0) && (resourceArray[2] === groupName)) {
-            returnArray.push(resourceArray[1]);
-          } else if ((typeof resourceArray[3] === 'string') && (resourceArray[3].length > 0) && (resourceArray[3] === groupId)) {
-            returnArray.push(resourceArray[1]);
-          } else {
-            returnArray.push(resourceArray[1]);
-          }
+    return new Promise((resolve, reject) => {
+      fs.readFile(jsonPath, 'utf8', (groupPermissionsErr, groupPermissionsData) => {
+        if (groupPermissionsErr) {
+          reject(`Error: ${groupPermissionsErr}`);
         }
+        const groupPermissions = JSON.parse(groupPermissionsData);
+        const returnArray = [];
+        Object.entries(groupPermissions).forEach(([resource, arrayOfGroupPermissionsArrays]) => {
+          arrayOfGroupPermissionsArrays.forEach((groupPermissionsArray) => {
+            if ((resource === resourceName) && (groupPermissionsArray[0] === groupResourceName)) {
+              const groupNameCheck = Boolean(groupPermissionsArray[2].length > 0) && (groupPermissionsArray[2] === groupName);
+              const groupIdCheck = Boolean(groupPermissionsArray[3].length > 0) && (groupPermissionsArray[3] === groupId);
+              if ((typeof groupPermissionsArray[2] === 'string') && (groupNameCheck === true)) {
+                returnArray.push(groupPermissionsArray[1]);
+              } else if ((typeof groupPermissionsArray[3] === 'string') && (groupIdCheck === true)) {
+                returnArray.push(groupPermissionsArray[1]);
+              } else {
+                returnArray.push(groupPermissionsArray[1]);
+              }
+            }
+          });
+        });
+        resolve(returnArray);
       });
-      return returnArray;
     });
   },
   getDbGroupPermissions(sequelize, resourceName, groupResourceName, groupName, groupId) {
@@ -37,38 +47,59 @@ export default {
       .then(permissionResults => permissionResults);
   },
   combinePermissions(permissionsArray, convertedPermissions) {
-    return permissionsArray.map((bit, index) => bit || convertedPermissions[index]);
+    let permissionsArrayReturn = permissionsArray;
+    if (permissionsArray.length === 0) {
+      permissionsArrayReturn = convertedPermissions;
+    }
+    return permissionsArrayReturn.map((bit, index) => bit || convertedPermissions[index]);
   },
   async accessCheck(testUserId, req, sequelize, resourceName, permissionsIndex) {
     let tempGroupPermissions;
     let unconvertedPermissionsArray = [];
     const userGroups = await this.getUserGroups(testUserId, req, sequelize);
 
-    return Promise.all(userGroups.map(async (group) => {
+    const combinedUnconvertedPermissions = await Promise.all(userGroups.map(async (group) => {
       tempGroupPermissions = await this.getJsonGroupPermissions(testUserId, resourceName, group.groupResourceName, group.groupName, group.groupID);
       unconvertedPermissionsArray = this.addToUnconvertedPermissionsArray(tempGroupPermissions, unconvertedPermissionsArray);
-      tempGroupPermissions = await this.getDbGroupPermissions(sequelize, resourceName, group.groupResourceName, group.groupName, group.groupId);
-      unconvertedPermissionsArray = this.addToUnconvertedPermissionsArray(tempGroupPermissions, unconvertedPermissionsArray);
-    })).then((unconvertedPermissions) => {
-      const convertedPermissionsArray = unconvertedPermissions.map(epilogueAuth.convertPermissions).reduce(this.combinePermissions, []);
-      return Boolean(convertedPermissionsArray.reduce(this.combinePermissions, [])[permissionsIndex]);
-    });
+      if (!testUserId) {
+        tempGroupPermissions = await this.getDbGroupPermissions(sequelize, resourceName, group.groupResourceName, group.groupName, group.groupId);
+        unconvertedPermissionsArray = this.addToUnconvertedPermissionsArray(tempGroupPermissions, unconvertedPermissionsArray);
+      }
+      return unconvertedPermissionsArray;
+    })).then(unconvertedPermissions => [].concat(...unconvertedPermissions));
+    const convertedPermissionsArray = combinedUnconvertedPermissions.map(epilogueAuth.convertPermissions, epilogueAuth);
+    return Boolean(convertedPermissionsArray.reduce(this.combinePermissions, [])[permissionsIndex]);
   },
   addToUnconvertedPermissionsArray(tempGroupPermissions, unconvertedPermissionsArray) {
+    let unconvertedPermissionsArrayReturn = unconvertedPermissionsArray;
     if (tempGroupPermissions) {
       if (typeof tempGroupPermissions === 'string') {
-        unconvertedPermissionsArray.push(tempGroupPermissions);
+        unconvertedPermissionsArrayReturn.push(tempGroupPermissions);
       } else if (Array.isArray(tempGroupPermissions) === true) {
-        unconvertedPermissionsArray.concat(tempGroupPermissions);
+        unconvertedPermissionsArrayReturn = unconvertedPermissionsArrayReturn.concat(tempGroupPermissions);
       }
     }
-    return unconvertedPermissionsArray;
+    return unconvertedPermissionsArrayReturn;
   },
   getUserGroups(testUserId, req, sequelize) {
-    const userId = (testUserId) || ((req || {}).user || {}).id;
-    let queryString = 'SELECT permission FROM "UserGroupXref"';
-    queryString += ` where "UserId" = '${userId}'`;
-    return sequelize.query(queryString, { type: sequelize.QueryTypes.SELECT })
-      .then(userGroupsResults => userGroupsResults);
+    const userId = ((req || {}).user || {}).id;
+    if (testUserId) {
+      return new Promise((resolve, reject) => {
+        fs.readFile('test/testUserGroups.json', 'utf8', (testUserGroupsErr, testUserGroupsData) => {
+          const testUserGroups = JSON.parse(testUserGroupsData);
+          if (testUserGroups) {
+            resolve(testUserGroups[testUserId]);
+          } else {
+            reject('Error in the testUserGroups.json file');
+          }
+        });
+      });
+    } else if (userId) {
+      let queryString = 'SELECT permission FROM "UserGroupXref"';
+      queryString += ` where "UserId" = '${userId}'`;
+      return sequelize.query(queryString, { type: sequelize.QueryTypes.SELECT })
+        .then(userGroupsResults => userGroupsResults);
+    }
+    return utilities.winstonWrapper('No user id and no test user id', 'info', false);
   },
 };
--- test/tests/groups/jsonPermissions.js
+++ test/tests/groups/jsonPermissions.js
@@ -5,83 +5,88 @@ if (process.env.NODE_ENV === 'staging' || process.env.NODE_ENV === 'production')
   srcOrBuild = 'src';
 }
 const groups = require(`../../../${srcOrBuild}/auth/groups`).default;
+const config = require(`../../../${srcOrBuild}/config`);
 import assert from 'assert';
-import testGroupPermissions from '../../testGroupPermissions.json';
+import Sequelize from 'sequelize';
+
+const database = new Sequelize(config.dbString);
 
 describe('Should have access to', () => {
-  it(`resource X, permission ${}`, done => {
-    assert.deepEqual(permissionReturnActual, permissionReturnExpected);
+  it('resource X, permission #', done => {
+    groups.accessCheck('testing', false, database, 'Todo', 5).then(accessResult => {
+      assert.deepEqual(accessResult, true);
+      done();
+    });
+  }).timeout(0);
+  it('resource X, permission #', done => {
+    assert.deepEqual(false, false);
     done();
   });
-  it(`resource X, permission ${}`, done => {
-    assert.deepEqual(permissionReturnActual, permissionReturnExpected);
+  it('resource X, permission #', done => {
+    assert.deepEqual(false, false);
     done();
   });
-  it(`resource X, permission ${}`, done => {
-    assert.deepEqual(permissionReturnActual, permissionReturnExpected);
+  it('resource Y, permission #', done => {
+    assert.deepEqual(false, false);
     done();
   });
-  it(`resource Y, permission ${}`, done => {
-    assert.deepEqual(permissionReturnActual, permissionReturnExpected);
+  it('resource Y, permission #', done => {
+    assert.deepEqual(false, false);
     done();
   });
-  it(`resource Y, permission ${}`, done => {
-    assert.deepEqual(permissionReturnActual, permissionReturnExpected);
+  it('resource Y, permission #', done => {
+    assert.deepEqual(false, false);
     done();
   });
-  it(`resource Y, permission ${}`, done => {
-    assert.deepEqual(permissionReturnActual, permissionReturnExpected);
+  it('resource Z, permission #', done => {
+    assert.deepEqual(false, false);
     done();
   });
-  it(`resource Z, permission ${}`, done => {
-    assert.deepEqual(permissionReturnActual, permissionReturnExpected);
+  it('resource Z, permission #', done => {
+    assert.deepEqual(false, false);
     done();
   });
-  it(`resource Z, permission ${}`, done => {
-    assert.deepEqual(permissionReturnActual, permissionReturnExpected);
-    done();
-  });
-  it(`resource Z, permission ${}`, done => {
-    assert.deepEqual(permissionReturnActual, permissionReturnExpected);
+  it('resource Z, permission #', done => {
+    assert.deepEqual(false, false);
     done();
   });
 });
 
 describe('Should not have access to', () => {
-  it(`resource X, permission ${}`, done => {
-    assert.deepEqual(permissionReturnActual, permissionReturnExpected);
+  it('resource X, permission #', done => {
+    assert.deepEqual(false, false);
     done();
   });
-  it(`resource X, permission ${}`, done => {
-    assert.deepEqual(permissionReturnActual, permissionReturnExpected);
+  it('resource X, permission #', done => {
+    assert.deepEqual(false, false);
     done();
   });
-  it(`resource X, permission ${}`, done => {
-    assert.deepEqual(permissionReturnActual, permissionReturnExpected);
+  it('resource X, permission #', done => {
+    assert.deepEqual(false, false);
     done();
   });
-  it(`resource Y, permission ${}`, done => {
-    assert.deepEqual(permissionReturnActual, permissionReturnExpected);
+  it('resource Y, permission #', done => {
+    assert.deepEqual(false, false);
     done();
   });
-  it(`resource Y, permission ${}`, done => {
-    assert.deepEqual(permissionReturnActual, permissionReturnExpected);
+  it('resource Y, permission #', done => {
+    assert.deepEqual(false, false);
     done();
   });
-  it(`resource Y, permission ${}`, done => {
-    assert.deepEqual(permissionReturnActual, permissionReturnExpected);
+  it('resource Y, permission #', done => {
+    assert.deepEqual(false, false);
     done();
   });
-  it(`resource Z, permission ${}`, done => {
-    assert.deepEqual(permissionReturnActual, permissionReturnExpected);
+  it('resource Z, permission #', done => {
+    assert.deepEqual(false, false);
     done();
   });
-  it(`resource Z, permission ${}`, done => {
-    assert.deepEqual(permissionReturnActual, permissionReturnExpected);
+  it('resource Z, permission #', done => {
+    assert.deepEqual(false, false);
     done();
   });
-  it(`resource Z, permission ${}`, done => {
-    assert.deepEqual(permissionReturnActual, permissionReturnExpected);
+  it('resource Z, permission #', done => {
+    assert.deepEqual(false, false);
     done();
   });
 });
